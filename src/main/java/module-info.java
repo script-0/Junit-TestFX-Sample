362f697f068e02230756e3056ecac09a79c37fda
script-0 <ndemabekolleemmanuelisaac@gmail.com>
2021-08-05 00:21:24 +0100

    Test run

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module com.script0.testfx {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
-    exports com.script0.testfx;
+    requires  javafx.graphics;
+    requires org.testfx.junit5;
+
+    exports com.script0.testfx to org.testfx.junit5, javafx.graphics;
+    opens com.script0.testfx to org.testfx.junit5,  javafx.graphics;
 }
